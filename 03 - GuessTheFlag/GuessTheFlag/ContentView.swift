d215ba3af091d6cafd38d5c7797eca41fb4a2a92
Nico <nmanograsso@gmail.com>
2022-05-06 16:19:38 -0300

    add: third project

---
+++ 03 - GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by nicolas.e.manograsso on 06/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 03 - GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by nicolas.e.manograsso on 06/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
