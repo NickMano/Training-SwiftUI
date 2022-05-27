e2aaa9e06acdc483274aac473a9994140dc85c18
Nico <nmanograsso@gmail.com>
2022-05-27 16:59:50 -0300

    add: init new app

---
+++ 04 - BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by nicolas.e.manograsso on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
